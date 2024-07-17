49227b06f35a93bcfe5581b357186b16632deb05
Naycen <aloulou.naycen@gmail.com>
2024-07-17 12:03:13 +0200

    adding font

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -19,6 +19,18 @@ class MainController extends AbstractController {
 
     }
 
+    /**
+     *@Route("/aboutUs", name="aboutus")
+     */
+
+    public function aboutUs() {
+
+        $contenuJson = file_get_contents('../data/team.json');
+        $contenu = json_decode($contenuJson);
+
+        return $this->render('main/about_us.html.twig',  array('content'=>$contenu, ));
+    }
+
 
 }
 
--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -6,15 +6,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wish", name="app_wish")
+ */
 class WishController extends AbstractController
 {
-    /**
-     * @Route("/wish", name="app_wish")
-     */
+
     public function index(): Response
     {
         return $this->render('wish/index.html.twig', [
             'controller_name' => 'WishController',
         ]);
     }
+
+
+    /**
+     *@Route("/list" , name ="_list")
+     */
+
+    public function List()
+    {
+        return $this->render('wish/list.html.twig', []);
+    }
+
+    /**
+     *@Route("/detail" , name ="_wish")
+     */
+    public function Detail()
+    {
+        return $this->render('wish/detail.html.twig', []);
+    }
+
 }
